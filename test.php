bf3aec4176a3d1775e87eca70626d2ffa2ea8e96
rafaesteller <esteller.rafa@gmail.com>
2018-07-15 23:22:35 +0200

    add behaviour to parent class - wrong implementation of hierarchy

--- test.php
+++ test.php
@@ -4,5 +4,5 @@ require_once "./vendor/autoload.php";
 
 $mountainBike = new MountainBike('S', 'rock shox reba', 'FOX');
 $roadBike = new RoadBike('56', 'green');
-var_dump($mountainBike);
-var_dump($roadBike);
+var_dump($mountainBike->spares());
+var_dump($roadBike->spares());
